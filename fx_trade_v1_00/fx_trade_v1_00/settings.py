ae8af3e15bbc0e66da79c439e12f4e50ae9edb27
Yuta Hidaka <yuta322@gmail.com>
2019-08-18 05:07:13 +0900

    s

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'fx_trade_v1_00.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'fx_trade',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fx_trade',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'yuta19940322',
+        'PASSWORD': 'souldout322',
         'HOST': 'localhost',
         'PORT': '3306',
     }
