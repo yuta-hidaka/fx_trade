550dd0c3880fb035a02f632aefd0ce2a4547d6fe
Yuta Hidaka <yuta322@gmail.com>
2019-08-18 03:22:54 +0900

    a

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,4 +131,3 @@ STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static"),
     ]
 )
-
