d96a03922ae5c9aa70bc608b8cd752a75a78894f
Yuta Hidaka <yuta322@gmail.com>
2019-08-18 03:42:55 +0900

    sou

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '2ok+j8kd*&b(h)6tc(bpvykt4f^qryr(1%a_53y*s3gyha5si3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['133.167.102.92']
 
 # Application definition
 
