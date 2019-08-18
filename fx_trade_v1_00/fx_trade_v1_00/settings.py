8b8c422f63c2a9db7411b96cc4a0981fa35493ce
Yuta Hidaka <yuta322@gmail.com>
2019-08-18 12:29:12 +0900

    d

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/python/static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/static')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = (
