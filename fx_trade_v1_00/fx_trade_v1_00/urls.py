4a43a5b29c5468025f3c7eafd148b1944e1c9966
Yuta Hidaka <yuta322@gmail.com>
2019-09-02 21:47:58 +0900

    gt

--- fx_trade_v1_00/fx_trade_v1_00/urls.py
+++ fx_trade_v1_00/fx_trade_v1_00/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('auto_trade/', include('auto_trade.urls')),
-    path('', include('auto_trade.urls')),
+    # path('', include('auto_trade.urls')),
     path('admin/', admin.site.urls),
 ]
