f23a737c1a4b4f320e8b0ebfe67e24f982b94858
Yuta Hidaka <yuta322@gmail.com>
2019-09-09 17:44:59 +0900

    adminにバッチ状況のtbl登録

--- fx_trade_v1_00/auto_trade/admin.py
+++ fx_trade_v1_00/auto_trade/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 # Register your models here.
-from .models import autoTradeOnOff
+from .models import autoTradeOnOff, batchRecord
+
+
 admin.site.register(autoTradeOnOff)
+admin.site.register(batchRecord)
