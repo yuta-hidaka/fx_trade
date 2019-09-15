f50409a32156f5cf947f849a52c3a0bfdced888c
yuta <yuta322@gmail.com>
2019-09-15 10:55:01 +0900

    管理画面にモデル追加

--- fx_trade_v1_00/auto_trade/admin.py
+++ fx_trade_v1_00/auto_trade/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 # Register your models here.
-from .models import autoTradeOnOff, batchRecord, M5_USD_JPY, MA_USD_JPY
+from .models import (autoTradeOnOff, batchRecord, M5_USD_JPY,
+                     MA_USD_JPY, SlopeM5_USD_JPY, conditionOfMA_M5,
+                     listConditionOfMA, listConditionOfSlope)
 
 
 admin.site.register(autoTradeOnOff)
 admin.site.register(batchRecord)
 admin.site.register(M5_USD_JPY)
 admin.site.register(MA_USD_JPY)
+
+admin.site.register(SlopeM5_USD_JPY)
+admin.site.register(conditionOfMA_M5)
+admin.site.register(listConditionOfMA)
+admin.site.register(listConditionOfSlope)
