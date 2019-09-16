7c6f867a4f953e0a27c76d669d72265a4b6d56d6
yuta <yuta322@gmail.com>
2019-09-16 21:06:50 +0900

    ｂｊｂｋｂｋ

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+admin.site.site_header = 'FX自動取引管理画面'
+admin.site.index_title = 'メニュー'
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
