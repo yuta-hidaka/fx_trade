5651f194e95c5dec0c386ce13e6cae2a11c1f112
yuta <yuta322@gmail.com>
2019-09-16 15:37:43 +0900

    vrvgrt

--- fx_trade_v1_00/auto_trade/service/set_MA_USD_JPY.py
+++ fx_trade_v1_00/auto_trade/service/set_MA_USD_JPY.py
@@ -56,15 +56,21 @@ class setMA_USD_JPY:
                   40, 50, 70, 72, 75, 140, 144, 150, 288]
         # 値比較
         comp = compaireMA()
+        print('hhdddgggggh')
+
         # 現在の最新MA一覧を取得する。
         leatestData = MA_USD_JPY.objects.latest('created_at')
+        print('hhh6')
+
         # 変数宣言
         vals = []
+        print('hhhddd')
 
         for ma in ListMa:
             data = list(M5_USD_JPY.objects.order_by(
                 '-recorded_at_utc')[:ma].aggregate(Avg('close')).values())
             vals.append(data[0])
+        print('hhh')
 
         qSet = MA_USD_JPY
         create = qSet.objects.create(
@@ -88,17 +94,23 @@ class setMA_USD_JPY:
             m5_ma288=vals[17]
         )
 
+        print('hhhkgkg')
+
         # 傾きも求める。
         self.setMASlope(leatestData, create)
         # 短中長期の状態を取得
+        print('hhhkgkrrrrg')
+
         # ma_comp5_20_75
         resultComp1 = comp.comp3MA(vals[0], vals[5], vals[13])
         # ma_comp6_24_72
         resultComp2 = comp.comp3MA(vals[1], vals[6], vals[12])
+        print('hhhkgkggggggggg')
 
         # 状態に関連するobject取得
         rComp1 = listConditionOfMA.objects.filter(id=resultComp1).first()
         rComp2 = listConditionOfMA.objects.filter(id=resultComp2).first()
+        print('hhhkgddddddddddddddddddkg')
 
         qSetCondition = conditionOfMA_M5
         qSetCondition.objects.create(
@@ -106,16 +118,17 @@ class setMA_USD_JPY:
             ma_comp6_24_72=rComp2,
             ma=create
         )
+        print('hhhdddddddddddddddddddddddddddddddddddddddddddddddddkgkg')
 
-        print('resultComp1')
-        print(vals[0])
-        print(vals[5])
-        print(vals[13])
-        print('resultComp1')
-        print(resultComp2)
-        print(vals[1])
-        print(vals[6])
-        print(vals[12])
+        # print('resultComp1')
+        # print(vals[0])
+        # print(vals[5])
+        # print(vals[13])
+        # print('resultComp1')
+        # print(resultComp2)
+        # print(vals[1])
+        # print(vals[6])
+        # print(vals[12])
 
-        print(leatestData)
-        print(create)
+        # print(leatestData)
+        # print(create)
