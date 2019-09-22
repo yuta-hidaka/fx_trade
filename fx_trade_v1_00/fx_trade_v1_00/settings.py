c38fd20cc418c1065d2a3ad085d0d834241dc50e
yuta <yuta322@gmail.com>
2019-09-22 11:48:29 +0900

    a

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = ['133.167.102.92', 'localhost', 'auto-trade.web-tool.tokyo']
 # Application definition
 
 INSTALLED_APPS = [
-    'auto_trade.apps.AutoTradeConfig',
     'auto_trade.rest.apps.RestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
