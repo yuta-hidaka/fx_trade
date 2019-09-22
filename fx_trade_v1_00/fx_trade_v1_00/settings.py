04ec4e99925f742369558eee5adbd71be2242a3a
yuta <yuta322@gmail.com>
2019-09-22 11:37:25 +0900

    l

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auto_trade',
     'rest_framework',
     'bootstrap4',
 ]
