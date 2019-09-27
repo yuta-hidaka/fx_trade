c955dbbf49d368fd1004070337f210dd04ed3c0c
yuta <yuta322@gmail.com>
2019-09-27 16:51:33 +0900

    ldl

--- fx_trade_v1_00/auto_trade/admin.py
+++ fx_trade_v1_00/auto_trade/admin.py
@@ -4,6 +4,7 @@ from .models import (
     autoTradeOnOff, batchRecord, M5_USD_JPY,
     MA_USD_JPY, SlopeM5_USD_JPY, conditionOfMA_M5, conditionOfSlope_M5,
     listConditionOfMA, listConditionOfSlope, condition, sellBuyAlert, orderStatus
+    ,bollingerBand
 )
 
 
@@ -11,6 +12,7 @@ class DateTime(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at', )
 
 
+admin.site.register(bollingerBand, DateTime)
 admin.site.register(orderStatus, DateTime)
 admin.site.register(autoTradeOnOff, DateTime)
 admin.site.register(sellBuyAlert, DateTime)
