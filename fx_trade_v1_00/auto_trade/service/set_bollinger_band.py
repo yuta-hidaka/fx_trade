073c9a903fa86a4da666e3a91670388ca94ce327
yuta <yuta322@gmail.com>
2019-10-17 01:56:31 +0900

    m

--- fx_trade_v1_00/auto_trade/service/set_bollinger_band.py
+++ fx_trade_v1_00/auto_trade/service/set_bollinger_band.py
@@ -307,11 +307,11 @@ class setBollingerBand_USD_JPY:
 
         if is_trend:
             # if is_trend and slopeDir != 0:
-            if is_plus:
+            if is_plus and slopeDir == 1 or slopeDir == 0:
                 text += '＋トレンド<br>'
                 # プラスのトレンド
                 trandCondi = 1
-            else:
+            elif not is_plus and slopeDir == -1 or slopeDir == 0:
                 text += '-トレンド<br>'
                 # マイナスのトレンド
                 trandCondi = 2
