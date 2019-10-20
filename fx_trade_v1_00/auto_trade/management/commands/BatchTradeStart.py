6a78b9873a08c3ae8583a83917afb3e8313c5d52
yuta <yuta322@gmail.com>
2019-10-21 08:29:28 +0900

    ll

--- fx_trade_v1_00/auto_trade/management/commands/BatchTradeStart.py
+++ fx_trade_v1_00/auto_trade/management/commands/BatchTradeStart.py
@@ -94,7 +94,7 @@ class Command(BaseCommand):
         # condiNow = setMA.setMA(result, BBCondi)
         # bsCal.BuySellCheck(condiNow, condiPrev)
 # '----------------デバッグ用-------------------------------'
-        if wk == 5 and hr == adjNum and mi == 59:
+        if wk == 5 and hr == adjNum and mi == 54:
             order.allOrderClose()
             text += '土曜日の終了時刻以降になったので取引中止処理を行います。<br>'
             is_closeMarket = True
