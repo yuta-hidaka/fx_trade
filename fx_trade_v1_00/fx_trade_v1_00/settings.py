018a82873982fafab88fa027c57e77fd35bef9a2
yuta <yuta322@gmail.com>
2019-10-22 23:06:31 +0900

    ooo

--- fx_trade_v1_00/fx_trade_v1_00/settings.py
+++ fx_trade_v1_00/fx_trade_v1_00/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'bootstrap4',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
