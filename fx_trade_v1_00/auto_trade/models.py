0d50d30e74364e87131b984408e7e1da875fd143
yuta <yuta322@gmail.com>
2019-10-22 23:25:53 +0900

    ooo

--- fx_trade_v1_00/auto_trade/models.py
+++ fx_trade_v1_00/auto_trade/models.py
@@ -243,6 +243,8 @@ class conditionOfBB(models.Model):
 class bollingerBand(models.Model):
     sma = models.DecimalField(
         max_digits=8, decimal_places=4, default=0.0000, null=True)
+    sma_4 = models.DecimalField(
+        max_digits=8, decimal_places=4, default=0.0000, null=True)
     abs_sigma_1 = models.DecimalField(
         max_digits=8, decimal_places=4, default=0.0000, null=True)
     abs_sigma_2 = models.DecimalField(
