eaea5da4a5f411be7bdd3c16319a030134c3f687
yuta <yuta322@gmail.com>
2019-10-23 21:01:55 +0900

    kldkdk

--- fx_trade_v1_00/auto_trade/service/set_bollinger_band.py
+++ fx_trade_v1_00/auto_trade/service/set_bollinger_band.py
@@ -226,23 +226,23 @@ class setBollingerBand_USD_JPY:
             rs = np.polyfit(x, y, 1)
             dcml = str(rs[0])
             slope_cv = Decimal(dcml).quantize(
-                Decimal('0.01'), rounding=ROUND_UP)
+                Decimal('0.01'), rounding=ROUND_DOWN)
             slopeDir = np.sign(slope_cv)
 
 # -----------------------------------------------------------------------
             slope_01 = Decimal(dcml).quantize(
-                Decimal('0.1'), rounding=ROUND_UP)
+                Decimal('0.1'), rounding=ROUND_DOWN)
             slope_001 = Decimal(dcml).quantize(
-                Decimal('0.01'), rounding=ROUND_UP)
+                Decimal('0.01'), rounding=ROUND_DOWN)
 
             slopeDir_01 = np.sign(slope_01)
             slopeDir_001 = np.sign(slope_001)
 # -----------------------------------------------------------------------
 
             s01 = Decimal('0.001039999999999003').quantize(
-                Decimal('0.1'), rounding=ROUND_UP)
+                Decimal('0.1'), rounding=ROUND_DOWN)
             s001 = Decimal('0.001039999999999003').quantize(
-                Decimal('0.01'), rounding=ROUND_UP)
+                Decimal('0.01'), rounding=ROUND_DOWN)
             text += ' test 0.01039999999999003を丸めた<br>'
             text += str(s01)+' 傾き<br>'
             text += str(s001)+' 傾き<br>'
