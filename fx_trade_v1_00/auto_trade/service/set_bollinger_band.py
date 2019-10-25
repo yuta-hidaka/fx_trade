d6b6c977ab0f14d4426925fa0058a44045c53429
yuta <yuta322@gmail.com>
2019-10-25 22:33:39 +0900

    pdpdp

--- fx_trade_v1_00/auto_trade/service/set_bollinger_band.py
+++ fx_trade_v1_00/auto_trade/service/set_bollinger_band.py
@@ -341,62 +341,62 @@ class setBollingerBand_USD_JPY:
         # if slopeDir == 0 and not is_trend:
         # text += '傾き0でトレンドじゃない=スクイーズの可能性<br>'
         # 小数第二以上でプラスであればエクスパンション
-        if slopeDir != 0:
-            if diff != Decimal(0):
-                is_expansion = True
-                is_expansionByNum = True
-                text += '価格差のエクスパンション<br>'
-            elif diff_2 != Decimal(0):
-                is_expansion = True
-                is_expansionByNum = True
-                text += '価格差のエクスパンション②<br>'
-            # elif sma2SigmaPlus <= nowClose and sma2SigmaPlus <= JNowClose and sma2SigmaPlus <= prevClose:
-            # elif sma2SigmaPlus <= nowClose and sma2SigmaPlus <= JNowClose:
-            # if sma2SigmaPlusEx <= nowClose and sma2SigmaPlusEx <= JNowClose:
-            if sma2SigmaPlusEx <= nowClose and sma2SigmaPlusEx_2 <= nowClose:
-                is_expansion = True
-                is_expansionByStd = True
-                is_topTouch = True
-                text += '上にエクスパンション<br>'
-            elif sma2SigmaPlusBeforEx <= bfClose and sma2SigmaPlusEx <= nowClose:
-                is_expansion = True
-                is_expansionByStd = True
-                is_topTouch = True
-                text += '上にエクスパンション(2回【終値】が超えた)<br>'
-            elif sma2SigmaPlusBeforEx <= bfHigh and sma2SigmaPlusEx <= nowHigh:
-                is_expansion = True
-                is_expansionByStd = True
-                is_topTouch = True
-                text += '上にエクスパンション(2回【高値】が超えた)<br>'
-            # elif sma2SigmaPlusEx_2 <= nowClose:
-            #     is_expansion = True
-            #     is_expansionByStd = True
-            #     is_topTouch = True
-            #     text += '上にエクスパンション②<br>'
-            # elif sma2SigmaMinus >= nowClose and sma2SigmaMinus >= JNowClose and sma2SigmaMinus >= prevClose:
-            # if sma2SigmaMinusEx >= nowClose and sma2SigmaMinusEx >= JNowClose:
-            if sma2SigmaMinusEx >= nowClose and sma2SigmaMinusEx_2 >= nowClose:
-                is_expansion = True
-                is_expansionByStd = True
-                is_bottomTouch = True
-                text += '下にエクスパンション<br>'
-            elif sma2SigmaMinusBeforEx >= bfClose and sma2SigmaMinusEx >= nowClose:
-                is_expansion = True
-                is_expansionByStd = True
-                is_bottomTouch = True
-                text += '下にエクスパンション(2回【終値】が超えた)<br>'
-            elif sma2SigmaMinusBeforEx >= bfLow and sma2SigmaMinusEx >= nowLow:
-                is_expansion = True
-                is_expansionByStd = True
-                is_bottomTouch = True
-                text += '下にエクスパンション(2回【底値】が超えた)<br>'
-            # elif sma2SigmaMinusEx_2 >= nowClose:
-            #     is_expansion = True
-            #     is_expansionByStd = True
-            #     is_bottomTouch = True
-            #     text += '下にエクスパンション②<br>'
-            # else:
-            #     is_expansion = False
+        # if slopeDir != 0:
+        if diff != Decimal(0):
+            is_expansion = True
+            is_expansionByNum = True
+            text += '価格差のエクスパンション<br>'
+        elif diff_2 != Decimal(0):
+            is_expansion = True
+            is_expansionByNum = True
+            text += '価格差のエクスパンション②<br>'
+        # elif sma2SigmaPlus <= nowClose and sma2SigmaPlus <= JNowClose and sma2SigmaPlus <= prevClose:
+        # elif sma2SigmaPlus <= nowClose and sma2SigmaPlus <= JNowClose:
+        # if sma2SigmaPlusEx <= nowClose and sma2SigmaPlusEx <= JNowClose:
+        if sma2SigmaPlusEx <= nowClose and sma2SigmaPlusEx_2 <= nowClose:
+            is_expansion = True
+            is_expansionByStd = True
+            is_topTouch = True
+            text += '上にエクスパンション<br>'
+        elif sma2SigmaPlusBeforEx <= bfClose and sma2SigmaPlusEx <= nowClose:
+            is_expansion = True
+            is_expansionByStd = True
+            is_topTouch = True
+            text += '上にエクスパンション(2回【終値】が超えた)<br>'
+        elif sma2SigmaPlusBeforEx <= bfHigh and sma2SigmaPlusEx <= nowHigh:
+            is_expansion = True
+            is_expansionByStd = True
+            is_topTouch = True
+            text += '上にエクスパンション(2回【高値】が超えた)<br>'
+        # elif sma2SigmaPlusEx_2 <= nowClose:
+        #     is_expansion = True
+        #     is_expansionByStd = True
+        #     is_topTouch = True
+        #     text += '上にエクスパンション②<br>'
+        # elif sma2SigmaMinus >= nowClose and sma2SigmaMinus >= JNowClose and sma2SigmaMinus >= prevClose:
+        # if sma2SigmaMinusEx >= nowClose and sma2SigmaMinusEx >= JNowClose:
+        if sma2SigmaMinusEx >= nowClose and sma2SigmaMinusEx_2 >= nowClose:
+            is_expansion = True
+            is_expansionByStd = True
+            is_bottomTouch = True
+            text += '下にエクスパンション<br>'
+        elif sma2SigmaMinusBeforEx >= bfClose and sma2SigmaMinusEx >= nowClose:
+            is_expansion = True
+            is_expansionByStd = True
+            is_bottomTouch = True
+            text += '下にエクスパンション(2回【終値】が超えた)<br>'
+        elif sma2SigmaMinusBeforEx >= bfLow and sma2SigmaMinusEx >= nowLow:
+            is_expansion = True
+            is_expansionByStd = True
+            is_bottomTouch = True
+            text += '下にエクスパンション(2回【底値】が超えた)<br>'
+        # elif sma2SigmaMinusEx_2 >= nowClose:
+        #     is_expansion = True
+        #     is_expansionByStd = True
+        #     is_bottomTouch = True
+        #     text += '下にエクスパンション②<br>'
+        # else:
+        #     is_expansion = False
 
         if not is_expansion:
             trendRatio = 90
