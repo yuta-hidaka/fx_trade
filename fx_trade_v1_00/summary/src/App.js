d9d8d60e250493e5f4779abf56d7eb1d9ca85de7
yuta <yuta322@gmail.com>
2019-11-10 16:13:21 +0900

    ;s;s;

--- fx_trade_v1_00/summary/src/App.js
+++ fx_trade_v1_00/summary/src/App.js
@@ -2,25 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>ReactApp作成中...</h1>
+        <h2>現在時刻 {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Clock />
     </div>
   );
 }
+
 
 export default App;
