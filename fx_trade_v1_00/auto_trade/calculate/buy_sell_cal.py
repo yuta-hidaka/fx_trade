de1da9689e234faefb68d88e40b6d1fe51ffd5f2
Your Name <you@example.com>
2019-11-19 20:24:14 +0900

    maでの購買を後ろに変更

--- fx_trade_v1_00/auto_trade/calculate/buy_sell_cal.py
+++ fx_trade_v1_00/auto_trade/calculate/buy_sell_cal.py
@@ -241,29 +241,6 @@ class BuySellCal():
 
                 # --------------------------------------------------------------------------------------------------------------------
             self.text += 'トレンドID　' + str(trend_id) + '<br>'
-            if maPrev == 6 or maPrev == 1 and maNow == 1 and slopeNow == 1:
-                if trend_id != 4:
-                    # print("long in by ma")
-                    self.text += "long in by ma<br>"
-                    self.order.LongOrderCreate()
-                else:
-                    # print("long in　but position is too many")
-                    self.text += "long in by ma trend idが4なので様子見です<br>"
-                    # shorのタイミング all slope is negative and befor MA is 3or4 and now 4
-                    
-            elif maPrev == 3 or maPrev == 4 and maNow == 4 and slopeNow == 2:
-                if trend_id != 4:
-                    # self.order.oderCloseAllLong()
-                    # print("short in by ma")
-                    self.text += "short in by ma<br>"
-                    self.order.ShortOrderCreate()
-                else:
-                    # print("short in　but position is too many")
-                    self.text += "short in by ma trend idが4なので様子見です<br>"
-                    # long closeのタイミング if MA is 2 it have to close
-            else:
-                # print('購買----様子見中')
-                self.text += '購買----様子見中 MAでの購買判定<br>'
 
     # --------------------------------------------------------------------------
             if trend_id == 1 or trend_id == 2 or trend_id == 4 and not is_peak:
@@ -481,6 +458,30 @@ class BuySellCal():
             #         self.text += "lossCutReverseで購入<br>"
             # else:
             #     self.text += "トレンドなのでlossCutReverseでの購入を行わない<br>"
+
+            if maPrev == 6 or maPrev == 1 and maNow == 1 and slopeNow == 1:
+                if trend_id != 4:
+                    # print("long in by ma")
+                    self.text += "long in by ma<br>"
+                    self.order.LongOrderCreate()
+                else:
+                    # print("long in　but position is too many")
+                    self.text += "long in by ma trend idが4なので様子見です<br>"
+                    # shorのタイミング all slope is negative and befor MA is 3or4 and now 4
+
+            elif maPrev == 3 or maPrev == 4 and maNow == 4 and slopeNow == 2:
+                if trend_id != 4:
+                    # self.order.oderCloseAllLong()
+                    # print("short in by ma")
+                    self.text += "short in by ma<br>"
+                    self.order.ShortOrderCreate()
+                else:
+                    # print("short in　but position is too many")
+                    self.text += "short in by ma trend idが4なので様子見です<br>"
+                    # long closeのタイミング if MA is 2 it have to close
+            else:
+                # print('購買----様子見中')
+                self.text += '購買----様子見中 MAでの購買判定<br>'
 
             self.text += '<br>-------------------------------ここからorderの内容----------------------------------<br>' + self.order.text
     # --------------------------------------------------------------------------------------------------------------------
