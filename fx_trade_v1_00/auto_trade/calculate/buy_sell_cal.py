20da73f2c9f6668ea096a48ad7bc58ae6e6d4f52
Your Name <you@example.com>
2019-12-02 08:50:02 +0900

    ko

--- fx_trade_v1_00/auto_trade/calculate/buy_sell_cal.py
+++ fx_trade_v1_00/auto_trade/calculate/buy_sell_cal.py
@@ -57,7 +57,8 @@ class BuySellCal():
 
         # 現在の為替情報とその5分10分前の為替の終値を取得する。
         nowCndl_close = Decimal(nowCndl['candles'][0]['mid']['c'])
-        self.priceNow = nowCndl_close
+        self.order.priceNow = nowCndl_close
+        self.order.LongOrderCreate()
         print(nowCndl_close)
         M5_1_closeNow = model_to_dict(condNow.ma.m5)['close']
         M5_1_closePrev = model_to_dict(condiPrev.ma.m5)['close']
