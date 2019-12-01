7517c26fb693b9c6f5629b2eff549dc6b4174a27
Your Name <you@example.com>
2019-12-02 01:49:41 +0900

    lpo

--- fx_trade_v1_00/summary/src/App.js
+++ fx_trade_v1_00/summary/src/App.js
@@ -15,7 +15,6 @@
 
 // import React from "react";
 import React, { Component } from "react";
-
 // import logo from "./logo.svg";
 import "./App.css";
 // import Button from "@material-ui/core/Button";
