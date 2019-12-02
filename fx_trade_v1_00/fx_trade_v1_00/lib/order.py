3265c11b0f79ac7edbdde6c28d59f86bab08ffc0
Your Name <you@example.com>
2019-12-02 21:09:29 +0900

    ko

--- fx_trade_v1_00/fx_trade_v1_00/lib/order.py
+++ fx_trade_v1_00/fx_trade_v1_00/lib/order.py
@@ -362,8 +362,8 @@ class orderFx:
         pNow = Decimal(self.priceNow)
         # print(slos)
         # print(pNow)
-        sld = slos - pNow
-        sld = np.abs(slos - pNow)
+        # sld = slos - pNow
+        sld = np.abs(slos - pNow) * -1
         if self.nowIn:
             self.text += 'short すでに購買済み<br>'
             return
