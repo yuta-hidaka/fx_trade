878081a8cc77a09767ff2f115215fe2203ccb736
Your Name <you@example.com>
2019-12-05 22:12:20 +0900

    ok

--- fx_trade_v1_00/auto_trade/calculate/buy_sell_cal.py
+++ fx_trade_v1_00/auto_trade/calculate/buy_sell_cal.py
@@ -100,14 +100,14 @@ class BuySellCal():
             pass
 
         if settings.use_specific_limit:
-            limit = settings.limit * 5
+            limit = settings.limit
             c = nowCndl_close
             long_limit = (c - (c*limit)).quantize(Decimal('0.001'),
                                                   rounding=ROUND_HALF_UP)
             short_limit = (
                 c + (c*limit)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
         else:
-            limit = sig1_2 * 5
+            limit = sig1_2
             long_limit = (sma_2 - limit).quantize(Decimal('0.001'),
                                                   rounding=ROUND_HALF_UP)
             short_limit = (
