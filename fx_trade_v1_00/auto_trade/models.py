d6e53386cf9d58880bad7f2423d834b3d0c8c708
Your Name <you@example.com>
2019-12-10 21:50:35 +0900

    ko

--- fx_trade_v1_00/auto_trade/models.py
+++ fx_trade_v1_00/auto_trade/models.py
@@ -443,12 +443,12 @@ class conditionOfMA_M5(models.Model):
 
 
 class condition(models.Model):
-    ma = models.ForeignKey(
-        'MA_USD_JPY', on_delete=models.CASCADE, related_name='condition_ma', null=True)
-    mas = models.ForeignKey(
-        'MA_Specific', on_delete=models.CASCADE, related_name='condition_ma2', null=True)
-    mas2 = models.ForeignKey(
-        'MA_Specific', on_delete=models.CASCADE, related_name='condition_ma3', null=True)
+    # ma = models.ForeignKey(
+    #     'MA_USD_JPY', on_delete=models.CASCADE, related_name='condition_ma', null=True)
+    # mas = models.ForeignKey(
+    #     'MA_Specific', on_delete=models.CASCADE, related_name='condition_ma2', null=True)
+    # mas2 = models.ForeignKey(
+    #     'MA_Specific', on_delete=models.CASCADE, related_name='condition_ma3', null=True)
     condition_of_slope_M5 = models.ForeignKey(
         'conditionOfSlope_M5', on_delete=models.CASCADE, related_name='conditionOfSlope_M5', null=True)
     condition_of_ma_M5 = models.ForeignKey(
